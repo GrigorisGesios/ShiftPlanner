868ccdb8c127948c824956367e3a9e82c6b8ff9d
ChristosKaragk <christoskaragkiavouridis@gmail.com>
2020-11-28 18:32:44 +0200

    Changes in Day class

--- app/src/main/java/com/example/shiftplanner/Day.java
+++ app/src/main/java/com/example/shiftplanner/Day.java
@@ -3,16 +3,38 @@ package com.example.shiftplanner;
 public class Day {
 
     private String numberOfShifts;
+    private String morningShift;
+    private String afternoonShift;
+    private String nightShift;
 
-    public Day(String numberOfShifts) {
+    public Day(String numberOfShifts, String morningShift, String afternoonShift, String nightShift) {
         this.numberOfShifts = numberOfShifts;
+        this.morningShift = morningShift;
+        this.afternoonShift = afternoonShift;
+        this.nightShift = nightShift;
     }
 
-    public String getNumberOfShifts() {
-        return numberOfShifts;
-    }
 
-    public void setNumberOfShifts(String numberOfShifts) {
-        this.numberOfShifts = numberOfShifts;
-    }
+    public String getNumberOfShifts() { return numberOfShifts; }
+
+    public void setNumberOfShifts(String numberOfShifts) { this.numberOfShifts = numberOfShifts; }
+
+    public String getMorningShift() { return morningShift; }
+
+    public void setMorningShift(String morningShift) { this.morningShift = morningShift; }
+
+    public String getAfternoonShift() { return afternoonShift; }
+
+    public void setAfternoonShift(String afternoonShift) { this.afternoonShift = afternoonShift; }
+
+    public String getNightShift() { return nightShift; }
+
+    public void setNightShift(String nightShift) { this.nightShift = nightShift; }
+
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/shiftplanner/Week.java
+++ app/src/main/java/com/example/shiftplanner/Week.java
@@ -2,27 +2,13 @@ package com.example.shiftplanner;
 
 public class Week {
 
-    private String morningShift;
-    private String afternoonShift;
-    private String nightShift;
+    private String maxDaysOff;
 
-    public Week(String morningShift, String afternoonShift, String nightShift) {
-        this.morningShift = morningShift;
-        this.afternoonShift = afternoonShift;
-        this.nightShift = nightShift;
+    public String getMaxDaysOff() {
+        return maxDaysOff;
     }
 
-    public String getMorningShift() { return morningShift; }
-
-    public void setMorningShift(String morningShift) { this.morningShift = morningShift; }
-
-    public String getAfternoonShift() { return afternoonShift; }
-
-    public void setAfternoonShift(String afternoonShift) { this.afternoonShift = afternoonShift; }
-
-    public String getNightShift() { return nightShift; }
-
-    public void setNightShift(String nightShift) { this.nightShift = nightShift; }
-
-
+    public void setMaxDaysOff(String maxDaysOff) {
+        this.maxDaysOff = maxDaysOff;
+    }
 }
