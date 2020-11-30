c8c078cf9b53ae8084dc4180036aa657e4164890
paporestis <73912183+paporestis@users.noreply.github.com>
2020-11-30 21:04:30 +0200

    JsonParsing(restrictions)

--- app/src/main/java/com/example/shiftplanner/MainActivity.java
+++ app/src/main/java/com/example/shiftplanner/MainActivity.java
@@ -1,60 +1,20 @@
 package com.example.shiftplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.os.Bundle;
-import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+import android.os.Bundle;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> restrictionlist = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        get_json();
     }
 
-    public void get_json()
-    {
-        String restrictions = null;
-        int i=1;
-        try
-        {
-            InputStream is = getAssets().open("Restrictions.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            restrictions = new String(buffer,"UTF-8");
 
-            JSONArray jsonArray = new JSONArray(restrictions);
-
-            JSONObject obj = jsonArray.getJSONObject(i);
-            {
-                restrictionlist.add(obj.getString("ar_vard"));
-                restrictionlist.add(obj.getString("prwi_pros"));
-                restrictionlist.add(obj.getString("apogeuma_pros"));
-                restrictionlist.add(obj.getString("vradu_pros"));
-                restrictionlist.add(obj.getString("sun_wres"));
-                restrictionlist.add(obj.getString("wres_evd"));
-                restrictionlist.add(obj.getString("max_adeia"));
-            }
-            Toast.makeText(getApplicationContext(),restrictionlist.toString(),Toast.LENGTH_LONG).show();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        catch (JSONException e)
-        {
-            e.printStackTrace();
-        }
-    }
 }
