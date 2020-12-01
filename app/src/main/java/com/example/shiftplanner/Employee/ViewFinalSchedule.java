603f8feb929bcee52e0907acc6fd0564a0304fc0
gregEsio <44166982+gregEsio@users.noreply.github.com>
2020-12-01 16:35:54 +0200

    Revert "Code cleanup"
    This reverts commit 6993378997e38ba569a30e004fbbc4014c121b17.

--- app/src/main/java/com/example/shiftplanner/Employee/ViewFinalSchedule.java
+++ app/src/main/java/com/example/shiftplanner/Employee/ViewFinalSchedule.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.shiftplanner.ParseJ;
 import com.example.shiftplanner.R;
 
 import org.json.JSONException;
@@ -15,5 +16,20 @@ public class ViewFinalSchedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_final_schedule);
+
+        //ΚΩΔΙΚΑΣ ΓΙΑ ΕΜΦΑΝΙΣΗ ΤΩΝ ΛΙΣΤΩΝ
+        TextView textView1 = (TextView) findViewById(R.id.EmployerView);
+        TextView textView2 = (TextView) findViewById(R.id.WorkersView);
+        TextView textView3 = (TextView) findViewById(R.id.ReqView);
+
+        ParseJ parsT = new ParseJ(this);
+        try {
+            parsT.ShowEmployers(textView1);
+            parsT.ShowWorkers(textView2);
+            parsT.ShowRequirements(textView3);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //ΚΩΔΙΚΑΣ ΓΙΑ ΕΜΦΑΝΙΣΗ ΤΩΝ ΛΙΣΤΩΝ
     }
 }
