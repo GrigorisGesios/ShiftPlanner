f9aceba9a8ff82d07c8d0ec42c6f862a7b98b96c
gregEsio <44166982+gregEsio@users.noreply.github.com>
2020-12-02 19:25:20 +0200

    Small changes

--- app/src/main/java/com/example/shiftplanner/MainActivity.java
+++ app/src/main/java/com/example/shiftplanner/MainActivity.java
@@ -1,21 +1,17 @@
 package com.example.shiftplanner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
-import android.widget.TextView;
-
-import org.json.JSONException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.shiftplanner.Employee.EmployeeLayout;
-import com.example.shiftplanner.Manager.ManagerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shiftplanner.Manager.*;
+import com.example.shiftplanner.Employee.*;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
