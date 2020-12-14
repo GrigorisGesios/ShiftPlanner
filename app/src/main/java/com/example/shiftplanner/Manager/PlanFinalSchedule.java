8f516028a2d11b9fb544ecb8ea3cc432fa66fb29
Nef99 <73385578+Nef99@users.noreply.github.com>
2020-12-14 21:58:53 +0200

    Έκδοση προγράμματος στο PlanFinalSchedule

--- app/src/main/java/com/example/shiftplanner/Manager/PlanFinalSchedule.java
+++ app/src/main/java/com/example/shiftplanner/Manager/PlanFinalSchedule.java
@@ -2,9 +2,18 @@ package com.example.shiftplanner.Manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.shiftplanner.R;
+import com.example.shiftplanner.ParseJ;
+import com.example.shiftplanner.Schedule;
+import com.example.shiftplanner.Shift;
+import com.example.shiftplanner.ToUI;
+import com.example.shiftplanner.Week;
+
+import org.json.JSONException;
 
 public class PlanFinalSchedule extends AppCompatActivity {
 
@@ -12,5 +21,14 @@ public class PlanFinalSchedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_final_schedule);
+
+        TextView textView1 = (TextView) findViewById(R.id.Text);
+
+        try {
+            Schedule scheduleobj = new Schedule();
+            scheduleobj.printSchedule(textView1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
