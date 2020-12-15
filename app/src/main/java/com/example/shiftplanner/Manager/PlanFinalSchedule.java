86246cdc7c03ee9812a81b3706ffec539f4530d5
Nef99 <73385578+Nef99@users.noreply.github.com>
2020-12-15 14:17:55 +0200

    Save schedule in txt file

--- app/src/main/java/com/example/shiftplanner/Manager/PlanFinalSchedule.java
+++ app/src/main/java/com/example/shiftplanner/Manager/PlanFinalSchedule.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.shiftplanner.R;
 import com.example.shiftplanner.ParseJ;
@@ -15,14 +17,20 @@ import com.example.shiftplanner.Week;
 
 import org.json.JSONException;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class PlanFinalSchedule extends AppCompatActivity {
+    private static final String FILE_NAME = "Schedule.txt";
+    TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_final_schedule);
 
-        TextView textView1 = (TextView) findViewById(R.id.Text);
+        textView1 = (TextView) findViewById(R.id.Text);
 
         try {
             Schedule scheduleobj = new Schedule();
@@ -30,5 +38,29 @@ public class PlanFinalSchedule extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public void save(View v) {
+        String text = textView1.getText().toString();
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            textView1.setText("");
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME, Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
