50f16d9e483c934ffeb57e6441cc381db53e75b0
gregEsio <44166982+gregEsio@users.noreply.github.com>
2020-12-22 01:36:53 +0200

    FireEmployee, btnShowEmployee, HireEmployee code changes

--- app/src/main/java/com/example/shiftplanner/Manager/HireEmployee.java
+++ app/src/main/java/com/example/shiftplanner/Manager/HireEmployee.java
@@ -5,16 +5,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shiftplanner.R;
 import com.example.shiftplanner.Workers;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class HireEmployee extends AppCompatActivity {
 
@@ -22,7 +18,6 @@ public class HireEmployee extends AppCompatActivity {
 
     Button btnConfirmHireEmployee;
     TextView edTextGiveName, edTextGiveProf, edTextGiveId,edTextGiveLastName;
-    Workers worker;
     long counter;
 
     @Override
@@ -35,32 +30,20 @@ public class HireEmployee extends AppCompatActivity {
         edTextGiveProf = (TextView) findViewById(R.id.edTextGiveProf);
         edTextGiveId = (TextView) findViewById(R.id.edTextGiveId);
         edTextGiveLastName = (TextView) findViewById(R.id.edTextGiveLastName);
-        worker = new Workers();
 
         database = FirebaseDatabase.getInstance().getReference().child("employee");
-
-        database.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    counter=(snapshot.getChildrenCount());
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
 
         btnConfirmHireEmployee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                worker.setFirstName(edTextGiveName.getText().toString().trim());
-                worker.setWorkersID(edTextGiveId.getText().toString().trim());
-                worker.setWorkersProf(edTextGiveProf.getText().toString().trim());
-                worker.setLastName(edTextGiveLastName.getText().toString().trim());
-                database.child(String.valueOf(counter)).setValue(worker);
+                String firstname = edTextGiveName.getText().toString();
+                String lastname = edTextGiveLastName.getText().toString();
+                String ID = edTextGiveId.getText().toString();
+                String idikotita = edTextGiveProf.getText().toString();
+
+                Workers worker = new Workers(firstname, lastname, ID, idikotita);
+
+                database.child(ID).setValue(worker);
             }
         });
     }
